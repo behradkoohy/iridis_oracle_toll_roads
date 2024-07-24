69c6458938c59d7b25fb17e84d99ef3dfeaf1966
Behrad Koohy <bk2g18@soton.ac.uk>
2024-07-24 22:10:34 +0100

    iridis found bugs lol

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,14 +2,14 @@
 
 dbpath="results.db"
 
-source bin/activate
+source venv/bin/activate
 
 python CreateDatabase.py $dbpath
 
 model_name="PSOTimeOnly"
 description="Calculate the optimal solution using PSO (Time Only)"
 vot_seed=0
-for i in {1..100}
+for i in {1..10}
 do
   sbatch experiment_daemon.slurm $dbpath $i $model_name $description $vot_seed $i
 done
